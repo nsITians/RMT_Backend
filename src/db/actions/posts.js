6a12f0d2cbac72d73ba0753138f0cabcc1d43fcc
tech4GT <varun.gupta1798@gmail.com>
2018-09-30 15:55:08 +0530

    add posts
    Signed-off-by: tech4GT <varun.gupta1798@gmail.com>

--- src/db/actions/posts.js
+++ src/db/actions/posts.js
@@ -2,6 +2,20 @@
  * Created by tech4GT on 9/30/18.
  */
 
-module.exports = {
+const models = require('../models');
 
+module.exports = {
+    add: function(data){
+        return models.Post.create(data);
+    },
+    edit: function (criteria,data) {
+        return models.Post.update(data,{
+            where: criteria
+        });
+    },
+    find: function (criteria) {
+        return models.Post.findAll({
+            where: criteria
+        });
+    }
 };
--- src/routes/api_v1/posts.js
+++ src/routes/api_v1/posts.js
@@ -4,4 +4,22 @@
 const router = require('express').Router(),
     actions = require('../../db/index').actions;
 
+
+router.post('/new',(req,res)=>{
+    const data = req.body;
+    actions.posts.add(data).then(data=>res.send(data));
+});
+
+router.get('/',(req,res)=>{
+    const filterCriteria = req.query;
+    actions.posts.find(filterCriteria).then(data=> res.send(data));
+});
+
+router.get('/:id',(req,res)=>{
+    const criteria = {
+        id: req.params.id
+    };
+    actions.posts.find(criteria).then(data=>res.send(data[0]));
+});
+
 module.exports = router;
